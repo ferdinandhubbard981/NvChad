52e86910bcadabe9eca823a8fd1049efaf895b49
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2023-07-11 13:52:42 +0200

    tried to source sdk, don't think it's working

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -3,8 +3,10 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
+local clangd_sdk_path = "/opt/sdk/camo/2.3.0.0/environment-setup-goldmont-camo-linux"  -- specify the path to your sdk
+
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { "html", "cssls", "tsserver" }  -- remove clangd from the list
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,5 +15,11 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- 
+-- setup clangd separately
+lspconfig.clangd.setup {
+  cmd = { "clangd", "--clang-tidy", "--compile-commands-dir=" .. clangd_sdk_path },
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
 -- lspconfig.pyright.setup { blabla}
